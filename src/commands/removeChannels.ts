470776ac74029017d4cfef4eaf2aecead6822077
Rushifaaa <jakubgencer@protonmail.com>
2020-11-12 22:30:47 +0100

    Rewrote remove command to work with new config

--- src/commands/removeChannels.ts
+++ src/commands/removeChannels.ts
@@ -1,42 +1,55 @@
 import { Message } from "discord.js";
+import { BotConfig, updateBotConfig } from "../config";
+import listChannels from "./listChannels";
 
-export default function removeChannels(args: string[], msg: Message): number {
+export default function removeChannels(args: string[], msg: Message, botConfig: BotConfig): number {
     if (args.length <= 0) {
         msg.channel.send("Please specify a channel to remove");
         return;
     }
 
-    // const config: Config = getConfig();
-    // if (!config) {
-    //     msg.channel.send("No bot Config found");
-    //     return;
-    // }
+    if (!botConfig) {
+        msg.channel.send("No bot Config found");
+        return;
+    }
 
-    // args.forEach(arg => {
-    //     if (!isNaN(Number(arg)) && Number(arg) !== 0) {
-    //         const indexOfArg = config.channels.indexOf(arg);
-    //         if (indexOfArg > -1) {
-    //             config.channels.splice(indexOfArg, 1);
-    //             const channel = msg.guild.channels.cache.get(arg);
-    //             msg.channel.send(`Channel with the ID: \`${arg} (${channel.name})\`, was successfully deleted from the Config.`);
-    //         } else {
-    //             console.log("Channel with ID: `" + arg + "`, was not found.");
-    //         }
-    //     } else {
-    //         const channel = msg.guild.channels.cache.find((channel, key) => channel.name === arg);
-    //         if (typeof (channel) !== "undefined") {
-    //             const indexOfArg = config.channels.indexOf(channel.id);
-    //             if (indexOfArg > -1) {
-    //                 config.channels.splice(indexOfArg, 1);
-    //                 msg.channel.send(`Channel with the ID: \`${channel.id} (${arg})\`, was successfully deleted from the Config.`);
-    //             }
-    //             return 0;
-    //         }
-    //         msg.channel.send("Channel with the Name: `" + arg + "`, was not found.");
-    //         return 0;
-    //     }
-    // });
+    let configChannelGroups = botConfig.channelGroups;
 
-    // updateConfig(config);
+    if (args.length === 1) {
+
+        let foundChannelGroup = configChannelGroups.find(channelGroup => channelGroup.name === args[0]);
+        if (typeof foundChannelGroup === "undefined") {
+            msg.channel.send("Cannot find channel group.");
+            return 0;
+        } else {
+            let indexOfGroupToDelete = configChannelGroups.indexOf(foundChannelGroup, 0);
+            configChannelGroups.splice(indexOfGroupToDelete, 1);
+
+            updateBotConfig(botConfig);
+            msg.channel.send(`The Channel group ${foundChannelGroup.name} was succsessfully deleted with all its channels.`);
+        }
+    } else {
+        let groupName = args.shift();
+        let foundChannelGroup = configChannelGroups.find(channelGroup => channelGroup.name === groupName);
+        if (typeof foundChannelGroup === "undefined") {
+            msg.channel.send("Cannot find channel group.");
+            return 0;
+        } else {
+            args.forEach(arg => {
+                let foundChannelToDelete = foundChannelGroup.channels.find(channel => channel.id === arg || channel.name === arg);
+                if (typeof foundChannelToDelete === "undefined") {
+                    msg.channel.send("This channel was not found in this group.");
+                    return 0;
+                }
+
+                let indexOfChannelToDelete = foundChannelGroup.channels.indexOf(foundChannelToDelete, 0);
+                foundChannelGroup.channels.splice(indexOfChannelToDelete, 1);
+                msg.channel.send(`The channel ${foundChannelToDelete.name}(${foundChannelToDelete.id}) was succsessfully deleted from channel group ${foundChannelGroup.name}.`);
+
+            });
+            updateBotConfig(botConfig);
+        }
+    }
+
     return 0;
 }
